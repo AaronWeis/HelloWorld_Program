bde421109149d498248fb76943d7084fbf0c1bb5
Aaron Weis <tuba.aaron@gmail.com>
2021-03-17 11:03:09 +0100

    added test stuff

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben Sie einen Namen ein:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hallo, " + name + "!");
+        }
+    }
+}
